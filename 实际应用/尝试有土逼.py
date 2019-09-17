35a7e94c7290dabc19599ede8dd1eef9a1c1a092
zhongkangjk <296053096@qq.com>
2019-09-17 23:38:50 +0800

    init

--- 实际应用/尝试有土逼.py
+++ 实际应用/尝试有土逼.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 from pprint import pprint
-url = 'https://youtu.be/VoSVmrwlvMU'
+url = 'https://www.youtube.com/watch?v=HK7SPnGSxLM'
 yt = YouTube(url)
 #yt.streams.filter(only_audio=True).all()   仅列出音频流
 #pprint(yt.streams.all())   #全部列表
@@ -8,10 +8,10 @@ yt = YouTube(url)
 #YouTube(url).streams.first().download()
 #video = yt.streams.filter('.mp4')[-1] #检索
 #pprint(yt.streams.all())
-#yt.streams.all()[-6].download()
-#audio = yt.streams.filter(only_audio=True).all()
-#pprint(audio)
-#audio[-1].download()
+#yt.streams.all()[-1].download()
+audio = yt.streams.filter(only_audio=True).all()
+pprint(audio)
+audio[0].download()
 
 
 
