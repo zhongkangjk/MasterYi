4b79350a4ff33ef34af58b3996f241442d3a15c3
zhongkangjk <296053096@qq.com>
2019-10-20 21:28:04 +0800

    scrapy小说

--- 爬虫框架scrapy/mySpider/mySpider/items.py
+++ 爬虫框架scrapy/mySpider/mySpider/items.py
@@ -11,4 +11,6 @@ import scrapy
 class MyspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    text = scrapy.Field()
+    #pass
--- 爬虫框架scrapy/mySpider/mySpider/pipelines.py
+++ 爬虫框架scrapy/mySpider/mySpider/pipelines.py
@@ -8,5 +8,5 @@
 
 class MyspiderPipeline(object):
     def process_item(self, item, spider):
-    	print(item['title'])
-        #return item
+        print(item["title"])
+        return item
