38c0a90745bea17a53b4783bef1a6e3d5839ba4a
zhongkangjk <296053096@qq.com>
2019-12-08 00:00:10 +0800

    又学习了一下django

--- Django/test1/booktest/urls.py
+++ Django/test1/booktest/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^index2$', views.index2),  # 建立/index和视图index之间的关系
     url(r'^books$',views.show_books), #显示图书信息
     url(r'^books/(\d+)$',views.detail),  #显示英雄信息
+    url(r'^login$',views.login,),  #显示登录页面
+    url(r'^login_check',views.login_check),# 用户登录校验
 ]
--- Django/test1/booktest/views.py
+++ Django/test1/booktest/views.py
@@ -2,24 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader,RequestContext
 from booktest.models import BookInfo  #导入图书模型类
+from django.shortcuts import redirect
 # Create your views here.
 # 1.定义视图函数，httpRequest
 # 2.进行url配置，建立url地址和视图的对应关系
 # http://127.0.0.1:8000/index
 def index(request):
-    '''
-    # 进行处理，和M和T进行交互
-    #return HttpResponse('老铁没毛病')
-    #使用一个模板文件
-    #1.加载模板文件
-    temp = loader.get_template('booktest/index.html')
-    #2.定义模板上下文：给模板文件传递数据
-    context = RequestContext(request,{})
-    # 3.模板渲染：产生标准的html内容
-    res_html = temp.render(context)
-    # 4.返回给浏览器
-    return HttpResponse(res_html)
-'''
+    '''首页'''
     return render(request,'booktest/index.html')
 
 
@@ -44,3 +33,27 @@ def detail(request,bid):
     # 3.使用模板
     return render(request,'booktest/detail.html',
                   {'book': book,'heros':heros})
+
+def login(request):
+    # 显示登录页面
+    if 'username' in request.COOKIES:
+        username = request.COOKIES['username']
+    else:
+        username = ''
+    return render(request , 'booktest/login.html',{'username':username})
+
+def login_check(request):
+    # 登录校验视图
+    # 1.获取提交的用户名和密码
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    remember = request.POST.get('remember')
+    print(remember)
+    # 2.进行登录的校验
+    if username == '123' and password == '123':
+        return redirect('/index')
+        if remember == 'on':
+            response.set_cookie('username',username,max_age=7*24*3600)
+    else:
+        return redirect('/login')
+    # 3.进行返回的应答
